ce6c727c829db9012947a526950270eee0bd4cc3
ariiqramadhan <ariiq.ramadhan@gmail.com>
2024-08-29 11:01:34 +0700

    feat: add delete wishlist handler

--- my-app/src/app/api/wishlist/route.ts
+++ my-app/src/app/api/wishlist/route.ts
@@ -1,4 +1,4 @@
-import { addWishlist } from "@/db/models/wishlist";
+import { addWishlist, delWishlist } from "@/db/models/wishlist";
 import { WishlistInput } from "@/types";
 import { ObjectId } from "mongodb";
 import { headers } from "next/headers";
@@ -24,6 +24,30 @@ export async function POST(req: NextRequest) {
             if (err.cause === 'NOT_FOUND') {
                 console.log(err.message);
                 return NextResponse.json({message: err.message}, { status: 404 });
+            }
+        }
+        return NextResponse.json({message: 'Internal Server Error'}, { status: 500 });
+    }
+}
+
+export async function DELETE(req: NextRequest) {
+    try {
+        const body = await req.json();
+        const userId = (headers().get('x-user-id')) as string;
+
+        const data = await delWishlist(new ObjectId(body.wishlistId), userId);
+
+    
+        return NextResponse.json(data);
+    } catch (err) {
+        if (err instanceof Error) {
+            if (err.cause === 'NOT_FOUND') {
+                console.log(err.message);
+                return NextResponse.json({message: err.message}, { status: 404 });
+            }
+            if (err.cause === 'FORBIDDEN') {
+                console.log(err.message);
+                return NextResponse.json({message: err.message}, { status: 403 });
             }
         }
         return NextResponse.json({message: 'Internal Server Error'}, { status: 500 });
--- my-app/src/db/models/wishlist.ts
+++ my-app/src/db/models/wishlist.ts
@@ -18,3 +18,20 @@ export const addWishlist = async (input: WishlistInput) => {
     const data = await wishlist.insertOne(input);
     return data;
 }
+
+export const delWishlist = async (wishlistId: ObjectId, userId: string) => {
+    const db = await getDB();
+    const wishlist = db.collection(WISHLIST_COLL)
+    const findWishlist = await wishlist.findOne({_id: wishlistId});
+    
+    if (!findWishlist) {
+        throw new Error('Wishlist not found!', { cause: 'NOT_FOUND' });
+    }
+    
+    if (String(findWishlist.userId) !== userId) {
+        throw new Error('You don\'t have privieleges to do this action', { cause: 'FORBIDDEN' });
+    }
+
+    const data = await wishlist.deleteOne({ _id: wishlistId });
+    return data;
+}
