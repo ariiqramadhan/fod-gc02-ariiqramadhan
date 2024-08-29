c56fc35a0b91d7c6ca91b0c9b6f0361f32f6e103
ariiqramadhan <ariiq.ramadhan@gmail.com>
2024-08-29 14:37:02 +0700

    feat: add login page design

--- my-app/src/app/(noauth)/layout.tsx
+++ my-app/src/app/(noauth)/layout.tsx
@@ -3,5 +3,5 @@ export default function RootLayout({
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-    return <div>{children}</div>;
+    return <>{children}</>;
 }
--- my-app/src/app/(noauth)/login/page.tsx
+++ my-app/src/app/(noauth)/login/page.tsx
@@ -1,5 +1,41 @@
+import { Metadata } from 'next';
+import Link from 'next/link';
+
+export const metadata: Metadata = {
+    title: 'Sign In to Fear Of Dog',
+    description: 'Sign In to your account for Fear Of Dog website'
+}
+
 export default function Login() {
     return (
-        <h1>Login</h1>
-    )
+        <div className="w-screen h-screen flex flex-col items-center justify-center gap-16">
+            <h1 className="text-5xl font-bold select-none">FEAR OF DOG</h1>
+            <div className="w-2/5">
+                <form action="" className="w-full flex flex-col gap-4">
+                    <h1 className="font-thin">LOGIN</h1>
+                    <input
+                        type="text"
+                        placeholder="USERNAME"
+                        className="font-thin text-sm border border-[#CCC] focus:border-[#000] focus:outline-none w-full p-2"
+                    />
+                    <input
+                        type="password"
+                        placeholder="PASSWORD"
+                        className="font-thin text-sm border border-[#CCC] focus:border-[#000] focus:outline-none w-full p-2"
+                    />
+                    <button className="btn btn-sm text-xs border-[#000] font-light rounded-none bg-[#FFF] shadow-none hover:bg-[#000] hover:text-[#FFF] hover:border-[#000]">
+                        SIGN IN
+                    </button>
+                    <h1 className="text-xs select-none w-fit cursor-pointer hover:text-[#ababab]">
+                        FORGOT YOUR PASSWORD?
+                    </h1>
+                </form>
+                <div className="w-full flex flex-col pt-16">
+                    <Link href='/register' className="btn btn-sm text-xs border-[#000] font-normal rounded-none bg-[#FFF] shadow-none hover:bg-[#000] hover:text-[#FFF] hover:border-[#000]">
+                            CREATE AN ACCOUNT
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
 }
