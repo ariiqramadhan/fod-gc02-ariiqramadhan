47590b169d9525ed2eddea66b49e1b6addf8592f
ariiqramadhan <ariiq.ramadhan@gmail.com>
2024-08-29 15:59:36 +0700

    feat: add register handler in client

--- my-app/src/app/(noauth)/register/page.tsx
+++ my-app/src/app/(noauth)/register/page.tsx
@@ -1,5 +1,8 @@
+import { BASE_URL } from "@/constant";
+import { UserInput } from "@/types";
 import { Metadata } from "next";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
     title: 'Sign Up to Fear Of Dog',
@@ -7,32 +10,65 @@ export const metadata: Metadata = {
 }
 
 export default function Register() {
+    async function handleRegister(formData: FormData) {
+        "use server"
+
+        const newUser = {
+            name: formData.get('name'),
+            username: formData.get('username') || null,
+            email: formData.get('email') || null,
+            password: formData.get('password') || null
+        }
+        const res = await fetch(BASE_URL + '/api/register', {
+            method: 'POST',
+            body: JSON.stringify(newUser),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        
+        const data = await res.json();
+
+        if (!res.ok) {
+            console.log(data);
+            return redirect(`/register?error=${data.message}`);
+        }
+        
+        return redirect('/login');
+    }
     return (
         <div className="w-screen h-screen flex flex-col items-center justify-center gap-16">
             <h1 className="text-5xl font-bold select-none">FEAR OF DOG</h1>
             <div className="w-2/5">
-                <form action="" className="w-full flex flex-col gap-4">
+                <form action={handleRegister} className="w-full flex flex-col gap-4">
                     <input
                         type="text"
+                        name="name"
                         placeholder="NAME"
                         className="font-thin text-sm border border-[#CCC] focus:border-[#000] focus:outline-none w-full p-2"
                     />
                     <input
                         type="text"
+                        name="username"
                         placeholder="USERNAME *"
                         className="font-thin text-sm border border-[#CCC] focus:border-[#000] focus:outline-none w-full p-2"
                     />
                     <input
                         type="text"
+                        name="email"
                         placeholder="EMAIL *"
                         className="font-thin text-sm border border-[#CCC] focus:border-[#000] focus:outline-none w-full p-2"
                     />
                     <input
                         type="password"
+                        name="password"
                         placeholder="PASSWORD *"
                         className="font-thin text-sm border border-[#CCC] focus:border-[#000] focus:outline-none w-full p-2"
                     />
-                    <button className="btn btn-sm text-xs border-[#000] font-light rounded-none bg-[#FFF] shadow-none hover:bg-[#000] hover:text-[#FFF] hover:border-[#000]">
+                    <button 
+                        className="btn btn-sm text-xs border-[#000] font-light rounded-none bg-[#FFF] shadow-none hover:bg-[#000] hover:text-[#FFF] hover:border-[#000]"
+                        type="submit"
+                    >
                         SIGN UP
                     </button>
                 </form>
--- my-app/src/constant.ts
+++ my-app/src/constant.ts
@@ -1,1 +1,2 @@
 export const DATABASE_NAME = "ecommerce"
+export const BASE_URL = "http://localhost:3000"
