8a6fe0238efdffad65a952c27e3d6e23bdb8b52e
ariiqramadhan <ariiq.ramadhan@gmail.com>
2024-09-01 17:46:20 +0700

    fix: fix products page design

--- my-app/src/components/ProductsData.tsx
+++ my-app/src/components/ProductsData.tsx
@@ -37,30 +37,42 @@ export default function ProductsData() {
             const products = await res.json();
             setData(data.concat(products.data));
             setPage(page + 1);
+            console.log('jalan nextdata');
         }, 500);
     }
 
     useEffect(() => {
-        getProducts({ search, page });
+        getProducts({ search: '', page: 1 });
     }, []);
+
     return (
         <>
-            <Search setSearch={setSearch} getProducts={getProducts} setData={setData}/>
-            <InfiniteScroll
-                dataLength={data?.length}
-                next={nextData}
-                hasMore={data?.length === totalData ? false : true}
-                loader={
-                    <div className='flex items-center justify-center w-screen'>
-                        <span className="loading loading-dots loading-lg"></span>
-                    </div>
-                }
-                className="grid grid-cols-3 px-16 gap-8 py-8"
-            >
-                {data?.map((product, i) => (
-                    <ProductCard product={product} key={`${product._id}`} />
-                ))}
-            </InfiniteScroll>
+            <Search
+                setSearch={setSearch}
+                getProducts={getProducts}
+                setData={setData}
+            />
+            {data.length ? (
+                <InfiniteScroll
+                    dataLength={data?.length}
+                    next={nextData}
+                    hasMore={data?.length === totalData ? false : true}
+                    loader={
+                        <div className="flex items-center justify-center w-screen">
+                            <span className="loading loading-dots loading-lg"></span>
+                        </div>
+                    }
+                    className="grid grid-cols-3 px-16 gap-8 py-8"
+                >
+                    {data?.map((product, i) => (
+                        <ProductCard product={product} key={`${product._id}`} />
+                    ))}
+                </InfiniteScroll>
+            ) : (
+                <div className="flex items-center justify-center w-screen">
+                    <span className="loading loading-dots loading-lg"></span>
+                </div>
+            )}
             {data?.length === totalData ? (
                 <div className="flex justify-center pb-8 font-semibold text-lg">
                     END OF THE PRODUCTS
