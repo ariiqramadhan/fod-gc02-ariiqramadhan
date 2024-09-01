1813b309a523945ee2a307cbea902a2be3805c1b
ariiqramadhan <ariiq.ramadhan@gmail.com>
2024-09-01 20:30:06 +0700

    fix: add try catch in getproductsbyid

--- my-app/src/app/api/products/[slug]/route.ts
+++ my-app/src/app/api/products/[slug]/route.ts
@@ -1,9 +1,14 @@
 import { getProductsById } from "@/db/models/product";
 
 export async function GET(req: Request, { params }: { params: { slug: string } }) {
-    const slug = params.slug.split('-');
-    const _id = slug[slug.length-1]
+    try {
+        const slug = params.slug.split('-');
+        const _id = slug[slug.length-1]
+    
+        const data = await getProductsById(_id);
+        return Response.json(data);
 
-    const data = await getProductsById(_id);
-    return Response.json(data);
+    } catch (err) {
+        return Response.json({message: 'Internal Server Error'}, {status: 500})
+    }
 }
